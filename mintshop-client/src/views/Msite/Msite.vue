f8f9a153e661415b755fd2a981d7963f9f8d8180
ZHAN_FENG <qq1140388042@gmail.com>
2020-11-11 23:15:50 +0800

    HeaderTop组件完成

--- mintshop-client/src/views/Msite/Msite.vue
+++ mintshop-client/src/views/Msite/Msite.vue
@@ -1,13 +1,203 @@
 <template>
-  <div>
-    App vue template
-  </div>
+  <section class="msite">
+    <!--首页头部title-->
+    <HeaderTop title="芝罘区鲁东大学北区(青年南路)">
+      <!-- 要使用slot="left"指定插入的插槽位置 -->
+      <span class="header_search" slot="left">
+        <i class="iconfont icon-sousuo"></i>
+      </span>
+      <span class="header_login" slot="right">
+        <span class="header_login_text">
+          登录|注册
+        </span>
+      </span>
+    </HeaderTop>
+    <!--首页导航轮播-->
+    <nav class="msite_nav">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container"></div>
+              <span>甜品饮品</span>
+            </a>
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container"></div>
+              <span>商超便利</span>
+            </a>
+            <!--下面的图片省略-->
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:" class="link_to_food">
+              <div class="food_container"></div>
+              <span>甜品饮品</span>
+            </a>
+            <!--同样省略-->
+          </div>
+        </div>
+        <!-- 轮播图页码 -->
+        <div class="swiper-pagination"></div>
+      </div>
+    </nav>
+    <!--首页附近商家列表-->
+    <div class="msite_shop_list">
+      <div class="shop_header">
+        <i class="iconfont icon-xuanxiang"></i>
+        <span class="shop_header_title">附近商家</span>
+      </div>
+      <div class="shop_container">
+        <ul class="shop_list">
+          <li class="shop_li border-1px">
+            <a>
+              <div class="shop_left"></div>
+              <div class="shop_right">
+                <section class="shop_detail_header">
+                  <h4 class="shop_title ellipsis">锄禾日当午，汗滴禾下土</h4>
+                  <ul class="shop_detail_ul">
+                    <li class="supports">保</li>
+                    <li class="supports">准</li>
+                    <li class="supports">票</li>
+                  </ul>
+                </section>
+                <section class="shop_rating_order">
+                  <section class="shop_rating_order_left">
+                    <div class="star star-24">
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item on"></span>
+                      <span class="star-item half"></span>
+                      <span class="star-item off"></span>
+                    </div>
+                    <div class="rating_section">
+                      3.6
+                    </div>
+                    <div class="order_section">
+                      月售106单
+                    </div>
+                  </section>
+                  <section class="shop_rating_order_right">
+                    <span class="delivery_style delivery_right">硅谷专送</span>
+                  </section>
+                </section>
+                <section class="shop_distance">
+                  <p class="shop_delivery_msg">
+                    <span>¥20起送</span>
+                    <span class="segmentation">/</span>
+                    <span>配送费约¥5</span>
+                  </p>
+                </section>
+              </div>
+            </a>
+          </li>
+          <!--省略其他店铺展示-->
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import HeaderTop from '@/components/HeaderTop/HeaderTop'
+
 export default {
   name: 'Msite',
+  components: {
+    HeaderTop,
+  },
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+@import "../../assets/stylus/mixins.styl"
+  .msite  //首页
+    width 100%
+    .header
+      background-color #02a774
+      position fixed
+      z-index 100
+      left 0
+      top 0
+      width 100%
+      height 45px
+      .header_search
+        position absolute
+        left 15px
+        top 50%
+        transform translateY(-50%)
+        width 10%
+        height 50%
+        .icon-sousuo
+          font-size 25px
+          color #fff
+      .header_title
+        position absolute
+        top 50%
+        left 50%
+        transform translate(-50%, -50%)
+        width 50%
+        color #fff
+        text-align center
+        .header_title_text
+          font-size 20px
+          color #fff
+          display block
+      .header_login
+        font-size 14px
+        color #fff
+        position absolute
+        right 15px
+        top 50%
+        transform translateY(-50%)
+        .header_login_text
+          color #fff
+    .msite_nav
+      bottom-border-1px(#e4e4e4)
+      margin-top 45px
+      height 200px
+      background #fff
+      .swiper-container
+        width 100%
+        height 100%
+        .swiper-wrapper
+          width 100%
+          height 100%
+          .swiper-slide
+            display flex
+            justify-content center
+            align-items flex-start
+            flex-wrap wrap
+            .link_to_food
+              width 25%
+              .food_container
+                display block
+                width 100%
+                text-align center
+                padding-bottom 10px
+                font-size 0
+                img
+                  display inline-block
+                  width 50px
+                  height 50px
+              span
+                display block
+                width 100%
+                text-align center
+                font-size 13px
+                color #666
+        .swiper-pagination
+          >span.swiper-pagination-bullet-active
+            background #02a774
+    .msite_shop_list
+            top-border-1px(#e4e4e4)
+            margin-top 10px
+            background #fff
+            .shop_header
+              padding 10px 10px 0
+              .shop_icon
+                margin-left 5px
+                color #999
+              .shop_header_title
+                color #999
+                font-size 14px
+                line-height 20px
+</style>
